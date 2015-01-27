06d92f47ba24d0d860035a06604a00f6f2d56ef0
Nitin Singh <extra.nitin@gmail.com>
2015-01-26 20:21:59 -0600

    Refactored

--- pathway_analysis.py
+++ pathway_analysis.py
@@ -15,17 +15,18 @@ import numpy as np
 from utils import *
 
 
-def score_pathways(path_df, other_df, data_type, opt=''):
+def score_pathways(data_df, data_type, opt=''):
     """ Score each pathway against a given gene by samples matrix of 
     mutation/mRNASeq/CNA data. Gene should be index of the data frame i.e.
     there should not be any column with gene ids.
     """
-    score_df = pd.DataFrame(index=path_df.columns, columns=other_df.columns)
+    path_df = preprocess_pathway_data()
+    score_df = pd.DataFrame(index=path_df.columns, columns=data_df.columns)
     for p in path_df.columns:
-        p_df = other_df.loc[path_df[p].dropna()]
+        # Get the genes that are present in data_df (gexp/cna/mut)
+        p_df = data_df.loc[path_df[p].dropna()]
         score_s = scorer(p_df, data_type, opt)
         score_df.loc[p] = score_s
-        
 
     return score_df
         
@@ -33,16 +34,17 @@ def scorer(df, data_type, opt):
     """ Call individual scoring functions that are specific to data type"""
     if data_type == 'mut':
         if opt == 'silent':
-            silent = df.replace([-1, 0], [np.nan, np.nan]).count()
+            silent = df.replace([-1, 0], [np.nan, np.nan]).count(axis=0)
             ret_s = silent/df.shape[0]
         elif opt == 'nsilent':
-            nsilent = df.replace([1, 0], [np.nan, np.nan]).count()
+            nsilent = df.replace([1, 0], [np.nan, np.nan]).count(axis=0)
             ret_s = nsilent/df.shape[0]
         elif opt == 'both':
             both = df.replace([0], [np.nan]).count()
             ret_s = both/df.shape[0]
         else:
             error('Unknown option %s in scorer' % opt)
+
         return ret_s
         
     if data_type == 'cna':
