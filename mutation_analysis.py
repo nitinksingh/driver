9af794e44fd63b0b40fa14909002061ed1117c61
Nitin Singh <extra.nitin@gmail.com>
2015-01-26 20:21:08 -0600

    Refactored

--- mutation_analysis.py
+++ mutation_analysis.py
@@ -15,6 +15,28 @@ from utils import *
 import clean_data as clean
 import pathway_analysis as pathway
 
+def generate_mutation_scores(can_type):
+    """ Compute and save pathway level mutation scores """
+    results_dir = '../results/' + can_type
+    makedirs(results_dir)
+    
+    # Load preprocessed mutation data    
+    data_dir = '../data/processed/' + can_type 
+    mut_path = data_dir + os.sep + can_type + '_mutation.txt'
+    mut_df = pd.read_table(mut_path, index_col=0, header=0)
+
+    # Compute pathway score for silent and non-silent mutations
+    silent_s = mut_df.replace([-1, 0], [np.nan, np.nan]).count()
+    nsilent_s = mut_df.replace([1, 0], [np.nan, np.nan]).count()
+
+    opt_s = dict(zip(['nsilent', 'silent'], [nsilent_s, silent_s]))   
+    
+    for opt, s in opt_s.iteritems():
+        score_df = pathway.score_pathways(path_df, mut_df, data_type='mut', opt=opt)
+        score_df.to_csv(results_dir + os.sep + opt +'_pathway_score.txt', index=True, index_label='Pathway', sep='\t')
+        print("\t Wrote " + opt +'_pathway_score.txt %d x %d' %sscore_df.shape )
+
+    
 def analyze_pathway_mutations(can_type, refresh=0):
     """ Compute correlation between pathway wise mutation scores and non-silent
     mutation count for the given patient population.
@@ -23,6 +45,7 @@ def analyze_pathway_mutations(can_type, refresh=0):
     result_file = 'pathway_mutation_score_vs_mutation_count.txt'
     if not os.path.exists(results_dir):
         os.makedirs(os.path.abspath(results_dir))
+    
     # Use previously generated calculations, if exists
     prefix = 'rho_' 
     result_path = results_dir + os.sep + prefix + result_file
@@ -40,14 +63,13 @@ def analyze_pathway_mutations(can_type, refresh=0):
     # in specific pathways
     silent_s = mut_df.replace([-1, 0], [np.nan, np.nan]).count()
     nsilent_s = mut_df.replace([1, 0], [np.nan, np.nan]).count()
-    both_s = mut_df.replace([0], [np.nan]).count()
 
     opt_s = dict(zip(['nsilent', 'silent'], [nsilent_s, silent_s]))   
     pathway_corr_df = pd.DataFrame(index=path_df.columns)
     for opt, s in opt_s.iteritems():
-        score_df = pathway.score_pathways(path_df, mut_df, data_type='mut', opt=opt)
-        score_df.to_csv(results_dir + os.sep + opt +'_pathway_score.txt', index=True, index_label='Pathway', sep='\t')
-        print("\t Wrote " + opt +'_pathway_score.txt %d x %d' %sscore_df.shape )
+        score_path = results_dir + os.sep + opt +'_pathway_score.txt'
+        score_df = pd.read_table(score_path, header=0, index_col=0) 
+
         for p in score_df.index:
             rho_pval = stats.spearmanr(score_df.loc[p], s)
             pathway_corr_df.loc[p, 'rho'+'_'+ opt] = rho_pval[0]
