397c8b95b5d5e13097497d1a1819b3cc8bd970c1
Nitin Singh <nitin.singh@utdallas.edu>
2015-03-06 10:43:17 -0600

    Bug fix

--- mutation_analysis.py
+++ mutation_analysis.py
@@ -19,19 +19,19 @@ def generate_mutation_scores():
     for can_type in CANCER_TYPES:
         results_dir = '../results/' + can_type
         makedirs(results_dir)
-        
-        # Load preprocessed mutation data    
-        data_dir = '../data/processed/' + can_type 
+
+        # Load preprocessed mutation data
+        data_dir = '../data/processed/' + can_type
         mut_categories = ['silent', 'nsilent']
-        
+
         path_df = pathway.preprocess_pathway_data()
-        
+
         for mut in mut_categories:
             mut_path = data_dir + os.sep + can_type + '_' + mut + '_mutation.txt'
             if not os.path.exists(mut_path):
                 print("processed mutation data is not found: %s" %can_type)
                 continue
-                
+
             mut_df = pd.read_table(mut_path, index_col=0, header=0)
             print can_type, mut_df.shape
             score_df = pathway.score_pathways(path_df, mut_df, data_type='mut')
@@ -40,14 +40,14 @@ def generate_mutation_scores():
             score_df.to_csv(results_dir + os.sep + mut +'_mutation_pathway_score.txt', index=True, index_label='Pathway', sep='\t', float_format='%.3f')
             print("Wrote " + can_type + " " + mut +'_mutation_pathway_score.txt %d x %d' %score_df.shape )
 
-    
+
 
 def get_pancancer_mutation_summary(agg_axis=0, refresh=False):
-    """ Summarize mutation data across cancer types. 
+    """ Summarize mutation data across cancer types.
     Parameters
     -----------------
     agg_axis: 0 or 1 Aggregate across Samples or Genes
-    refresh: True/False Recompute mutation frequencies from all cancer types 
+    refresh: True/False Recompute mutation frequencies from all cancer types
                 or reload from previous computaion
     """
     outdir = '../data/pancancer'
@@ -58,39 +58,30 @@ def get_pancancer_mutation_summary(agg_axis=0, refresh=False):
     else:
         ns_outf = outdir + os.sep + 'non_silent_mutations_samples.txt'
         s_outf = outdir + os.sep + 'silent_mutations_samples.txt'
-    
+
     if os.path.exists(ns_outf) and not refresh:
         ns_summary = pd.read_table(ns_outf, sep='\t', index_col=0, header=0)
         s_summary = pd.read_table(s_outf, sep='\t', index_col=0, header=0)
-        
+
         return (ns_summary, s_summary)
-        
-        
-    cohort = "ACC BLCA BRCA CESC CHOL COAD COADREAD DLBC ESCA FPPP GBM GBMLGG HNSC KICH KIRC KIRP LAML LGG LIHC LUAD LUSC MESO OV PAAD PRAD READ SARC SKCM STAD TGCT THCA THYM UCEC UCS UVM"
+
+
     ns_summary = pd.DataFrame()
     s_summary = pd.DataFrame()
-    for can in cohort.split():
+    for can in CANCER_TYPES:
         f1 = '../data/processed/' + can + os.sep + can + '_nsilent_mutation.txt'
         f2 = '../data/processed/' + can + os.sep + can + '_silent_mutation.txt'
         if not os.path.exists(f1) or not os.path.exists(f2):
             #print("Skipping %s " %can)
             continue
 
-        # Read non-silent mutations and convert mutations to 1 from -1
+        # Read non-silent mutations and compute mutation frequencies
         nsdf = pd.read_table(f1, header=0, index_col=0)
         nsilent = nsdf.sum(axis=agg_axis)
-        
+
         sdf = pd.read_table(f2, header=0, index_col=0)
         silent = sdf.sum(axis=agg_axis)
 
-        if '.' in sdf.index:
-            print can
-
-        # Compute mutation frequencies (%) of gene across samples
-        if agg_axis == 1:
-            silent = silent*100/sdf.shape[1]
-            nsilent = nsilent*100/nsdf.shape[1]
-        
         if ns_summary.empty:
             ns_summary[can] = nsilent
             s_summary[can] = silent
@@ -98,17 +89,16 @@ def get_pancancer_mutation_summary(agg_axis=0, refresh=False):
             ns_summary = ns_summary.join(pd.Series(nsilent, name=can), how='outer')
             s_summary = s_summary.join(pd.Series(silent, name=can), how='outer')
 
-    
+
     # Save for future usage
-    ns_summary = ns_summary.applymap(lambda x: '%.3f' %x)
-    s_summary = s_summary.applymap(lambda x: '%.3f' %x)
     ns_summary.to_csv(ns_outf, sep='\t', header=True, index=True, float_format='%.3f')
     s_summary.to_csv(s_outf, sep='\t', header=True, index=True, float_format='%.3f')
-    print("Wrote %s %d x %d matrix" %(os.path.basename(ns_outf), len(ns_summary.index), len(ns_summary.columns)))          
-    print("Wrote %s %d x %d matrix" %(os.path.basename(s_outf), len(s_summary.index), len(s_summary.columns)))          
-    
+    print("Wrote %s %d x %d matrix" %(os.path.basename(ns_outf), len(ns_summary.index), len(ns_summary.columns)))
+    print("Wrote %s %d x %d matrix" %(os.path.basename(s_outf), len(s_summary.index), len(s_summary.columns)))
+
     return (ns_summary, s_summary)
 
 if __name__ == '__main__':
     """ Gen pathway mutation correlation with non-silent mutation counts"""
-    generate_mutation_scores()        
+    #generate_mutation_scores()
+    get_pancancer_mutation_summary(0, True)
