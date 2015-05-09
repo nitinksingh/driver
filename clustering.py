bbae9b923ddbb161501eeaa1bc3dd060d6cdd29c
Nitin Singh <extra.nitin@gmail.com>
2015-05-08 19:02:05 -0500

    plot heatmap of pathway score and gene mutations

--- clustering.py
+++ clustering.py
@@ -19,6 +19,7 @@ from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.cluster.bicluster import SpectralCoclustering, SpectralBiclustering
 
 import sklearn.metrics as metrics
+import pathway_analysis as pathway
 
 # Adopted from to OxanaSachenkova/hclust-python at github
 def hierarchical_clustering(data_array, labels=''):
@@ -167,6 +168,91 @@ def get_data(can='LUAD', data_type='mut', nsdf_norm_factor=1):
 
     return (sig_nsdf, clinical_df)
 
+
+# Adopted from to OxanaSachenkova/hclust-python at github
+def combined_heatmap(cancer_type, num_pathways=5, num_genes=10, groups=None):
+    
+    # Load preprocessed mutation data
+    data_dir = '../data/processed/' + cancer_type
+    path_df = pathway.preprocess_pathway_data()
+    mut_path = data_dir + os.sep + cancer_type + '_nsilent_mutation.txt'
+    mut_df = pd.read_table(mut_path, index_col=0, header=0)
+
+    # Load mutation data aggregated at the level of pathways
+    (sig_nsdf, clinical_df) = get_data(cancer_type, 'mut', nsdf_norm_factor=1)
+    clinical_df = clinical_df.loc[clinical_df.time > 0]
+    common_samples = sig_nsdf.columns.intersection(clinical_df.index)
+    sig_nsdf = sig_nsdf[common_samples]
+    clinical_df = clinical_df.loc[common_samples] # redaundant as of now
+    df = sig_nsdf.iloc[:num_pathways]
+    labels = sig_nsdf.columns
+
+    # Do heirarchical clustering on samples
+    data_array = df.transpose().values
+    if type(labels) == str:
+        labels = map(str, range(1, data_array.shape[0]+1))
+    data_dist = pdist(data_array) # computing the distance
+    Y = linkage(data_dist, method='single')
+
+    x0 = 0.2; w=0.7; y00 = 0.93; y01 = 0.8; y = 25
+    # Compute and plot first dendrogram.
+    fig = plt.figure(figsize=(20,y))
+    ax2 = fig.add_axes([x0,y00,w,0.06])
+    Z2 = dendrogram(Y,labels=labels, leaf_font_size=8)#, color_list=color_list.values)
+    idx2 = Z2['leaves']
+    ax2.set_yticks([])
+   
+    # Cluster Ids
+    ax3 = fig.add_axes([x0,y01+0.1005,w,0.002])
+    if type(groups) is type(None):
+        clust_mat = 10*np.ones((1, df.shape[1]))
+    else:
+        clust_mat = np.reshape(groups.iloc[idx2].values, (1,len(groups)))
+
+    im = ax3.matshow(clust_mat, aspect='auto', origin='upper', cmap='Oranges')
+    ax3.set_yticks([])
+    ax3.set_xticks([])
+
+    #Compute and plot the heatmap
+    axmatrix = fig.add_axes([x0,y01,w,0.1])
+    D = df.loc[:,df.columns[idx2]].values
+    im = axmatrix.matshow(D, aspect='auto', origin='upper', cmap=plt.cm.YlGnBu)
+  
+    axmatrix.set_yticklabels([''] + list(df.index), rotation=0)
+    axmatrix.set_xticks([])
+
+    
+    # Plot colorbar.
+    axcolor = fig.add_axes([0.91,y01,0.02,0.1])
+    plt.colorbar(im, cax=axcolor)
+    
+    cmaps = ['Oranges', 'binary', 'Spectral']
+    h = y01/df.shape[0] - df.shape[0]*0.01
+    for i in range(df.shape[0]):
+        y0 = y01-h*(i+1)
+    
+        # Plot individual mutation
+        ax3 = fig.add_axes([x0,y0,w, h] )
+        path_genes = path_df[df.index[i]].dropna()
+        path_genes_df = mut_df.loc[path_genes, df.columns[idx2]].fillna(0)
+        path_genes_df = path_genes_df.loc[path_genes_df.sum(axis=1) != 0, :]
+        sorted_df = path_genes_df.sum(axis=1).order(ascending=False)
+        path_genes_df = path_genes_df.loc[sorted_df.index[:num_genes]]
+#         print D.shape, path_genes_df.shape, df.index[i]
+        D2 = path_genes_df.values
+        im2 = ax3.matshow(D2, aspect='auto', origin='upper', cmap=cmaps[i%len(cmaps)])
+        ax3.set_yticks(range(len(path_genes_df.index)))
+        ax3.set_yticklabels(list(path_genes_df.index), fontsize=9)
+        ax3.set_xticks([])
+        # Plot colorbar.
+        axcolor2 = fig.add_axes([0.91,y0,0.02,h])
+        plt.colorbar(im2, cax=axcolor2)
+        
+#     print path_genes_df.head()
+    plt.show()
+
+    return idx2
+    
 if __name__ == "__main__":
     (sig_nsdf, clinical_df) = get_data()
     X = sig_nsdf[:50].transpose().values
